fd0d959b1a58b0d47c40f805f71ff97ddf305143
MarcusViniF0 <marcussouzatrab@gmail.com>
2022-09-10 20:04:56 -0300

    Primeiro commit

---
+++ projetoteste1/src/projetoteste1/classeExecultavel.java
@@ -0,0 +1,17 @@
+package projetoteste1;
+
+public class classeExecultavel {
+
+	public static void main(String[] args) {
+		
+		
+		Aluno aluno=new Aluno();
+		
+		aluno.setNome("Jossana");
+		aluno.setIdade(32);
+		
+		System.out.println("Nome: "+aluno.getNome());
+		System.out.println("Nome: "+aluno.getIdade());
+	}
+
+}
